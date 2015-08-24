8f0cba627171b9801f2be9a85d86a3bfdcc2fd7a
Eric Andre <eric@meetidesign.com>
2015-08-24 14:56:25 -0500

    add regex for github feed links

--- public/_assets/js/main.js
+++ public/_assets/js/main.js
@@ -1195,19 +1195,22 @@ if(typeof module !== 'undefined' && module.exports) {
 window.addEventListener('load', function() {
     var gitfeed;
     var feedContainer = document.getElementById('feed');
+    var gitRegEx = /(<a.*?href\s*=\s*[\"'])\s*/ig;
+    var gitURL = "https://github.com";
 
     function feedBuilder(feed, limit) {
-        var limit = limit || 3;
-        
+        limit = limit || 3;
+
         for(var i = 0; i < limit; i++) {
             var elem = document.createElement('div');
+            var processedFeed = feed[i].content[0]._.replace(gitRegEx, "$1"+gitURL);
             elem.classList.add('alert');
-            elem.innerHTML = feed[i].content[0]._;
+            elem.innerHTML = processedFeed;
             feedContainer.appendChild(elem);
         }
 
         return;
-    };
+    }
 
     kickback.request({
         url: '/feed',
@@ -1216,14 +1219,14 @@ window.addEventListener('load', function() {
     .then(function(response) {
         gitfeed = JSON.parse(response);
         var entries = gitfeed.feed.entry;
-        
+
         feedBuilder(entries, 6);
-        
+
         return;
     })['catch'](function(err) {
         return err;
     });
-    
+
 }, false);
 
 (function(window) {
